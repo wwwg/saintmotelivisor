b546a598561114898021d674dc69a2d2370756ca
ultratypebot <xxperipsis@gmail.com>
2018-08-22 21:10:27 -0500

    write email function

--- saintmotelivisor.js
+++ saintmotelivisor.js
@@ -1,4 +1,25 @@
 const request = require('request'),
 	nodemailer = require('nodemailer'),
 	EMAIL_ADDR = 'joseph.goolag@gmail.com',
-	EMAIL_PASS = 'goolag66';
+	EMAIL_PASS = 'goolag66',
+	transport = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+			user: EMAIL_ADDR,
+			pass: EMAIL_PASS
+		}
+	});
+let email = (to, subject, body) => {
+	let opts = {
+		'from': EMAIL_ADDR,
+		'to': to,
+		'subject': subject,
+		'text': body
+	}
+	transport.sendMail(opts,(err, info) => {
+		if (error) {
+			console.log('ERROR sending email:');
+			console.log(err);
+		}
+	});
+}
