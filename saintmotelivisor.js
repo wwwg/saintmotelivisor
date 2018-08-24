41865e7178d6087bdf8465484e21fc508622afb7
wwwwwwwwwwwwwwwwwwwwwwwwwwwwww <xxperipsis@gmail.com>
2018-08-24 10:55:34 -0500

    add init exporter

--- saintmotelivisor.js
+++ saintmotelivisor.js
@@ -31,52 +31,58 @@ if (!process.argv[2]) {
 	process.exit(0);
 }
 let isRequestInProgress = false,
-	lastBody = null,
-	emails = process.argv.slice(2);;
-
-setInterval(() => {
-	if (isRequestInProgress) {
-		console.log('WARN: skipping request cycle because prior request isn\'t complete');
-		return;
-	}
-	isRequestInProgress = true;
-	request(CONCERT_QUERY_ENDPOINT, (err, res, body) => {
-		if (lastBody === null) {
-			console.log('recieved initial body:');
-			console.log(body);
-			lastBody = body;
-			let res = JSON.parse(body),
-				emailBody = 'Current concerts:\n',
-				concerts = res.resultsPage.results.performance, // an array
-				length = (res.resultsPage.performance > 3) ? 3 : res.resultsPage.results.performance.length;
-			for (let i = 0; i <  length; ++i) {
-				emailBody += `#${i + 1}: ${res.resultsPage.results.performance[i].event.displayName}\n`;
-			}
-			for (let i = 0; i < emails.length; ++i) {
-				email(emails[i], 'saintmotelivisor concert listing', emailBody);
-			}
-		} else if (body == lastBody) {
-			// console.log('recieved identical body, nothing special has occured.');
-		} else if (body != lastBody) {
-			let res;
-			try {
-				JSON.parse(body);
-			} catch (e) {
-				console.log('recieved wrong response: ');
-				console.log(body);
-				return;
-			}
-			console.log('change detected, emailing recipient');
-			let emailBody = 'New concerts:\n',
-				concerts = res.resultsPage.results.performance, // an array
-				length = (res.resultsPage.performance > 3) ? 3 : res.resultsPage.results.performance.length;
-			for (let i = 0; i <  length; ++i) {
-				emailBody += `#${i + 1}: ${res.resultsPage.results.performance[i].event.displayName}\n`;
-			}
-			for (let i = 0; i < emails.length; ++i) {
-				email(emails[i], 'Saint Motel Concert Update', emailBody);
-			}
+	lastBody = null;
+let init = (emails) => {
+	setInterval(() => {
+		if (isRequestInProgress) {
+			console.log('WARN: skipping request cycle because prior request isn\'t complete');
+			return;
 		}
-		isRequestInProgress = false;
-	});
-}, DEFAULT_QUERY_INTERVAL);
+		isRequestInProgress = true;
+		request(CONCERT_QUERY_ENDPOINT, (err, res, body) => {
+			if (lastBody === null) {
+				console.log('recieved initial body:');
+				console.log(body);
+				lastBody = body;
+				let res = JSON.parse(body),
+					emailBody = 'Current concerts:\n',
+					concerts = res.resultsPage.results.performance, // an array
+					length = (res.resultsPage.performance > 3) ? 3 : res.resultsPage.results.performance.length;
+				for (let i = 0; i <  length; ++i) {
+					emailBody += `#${i + 1}: ${res.resultsPage.results.performance[i].event.displayName}\n`;
+				}
+				for (let i = 0; i < emails.length; ++i) {
+					email(emails[i], 'saintmotelivisor concert listing', emailBody);
+				}
+			} else if (body == lastBody) {
+				// console.log('recieved identical body, nothing special has occured.');
+			} else if (body != lastBody) {
+				let res;
+				try {
+					JSON.parse(body);
+				} catch (e) {
+					console.log('recieved wrong response: ');
+					console.log(body);
+					return;
+				}
+				console.log('change detected, emailing recipient');
+				let emailBody = 'New concerts:\n',
+					concerts = res.resultsPage.results.performance, // an array
+					length = (res.resultsPage.performance > 3) ? 3 : res.resultsPage.results.performance.length;
+				for (let i = 0; i <  length; ++i) {
+					emailBody += `#${i + 1}: ${res.resultsPage.results.performance[i].event.displayName}\n`;
+				}
+				for (let i = 0; i < emails.length; ++i) {
+					email(emails[i], 'Saint Motel Concert Update', emailBody);
+				}
+			}
+			isRequestInProgress = false;
+		});
+	}, DEFAULT_QUERY_INTERVAL);
+}
+module.exports = init;
+
+if (require.main === module) {
+	console.log('saintmotelivisor cmd line');
+	init(process.argv.slice(2));
+}
