869bcac12f1fd0a47f8835749c1b18102a818316
wwwwwwwwwwwwwwwwwwwwwwwwwwwwww <xxperipsis@gmail.com>
2018-08-24 11:04:13 -0500

    finish daemon installation

--- mkdaemon.sh
+++ mkdaemon.sh
@@ -4,3 +4,13 @@ if [ ! -d node_modules ]; then
 	echo "installing modules"
 	npm i
 fi
+if ! [ $(id -u) = 0 ]; then
+   echo "you must run this script as root"
+   exit 1
+fi
+mkdir -p /etc/saintmotelivisord
+cp -r . /etc/saintmotelivisord
+
+cp ./daemon/saintmotelivisord.service /etc/systemd/system
+systemctl start saintmotelivisord
+systemctl enable saintmotelivisord
