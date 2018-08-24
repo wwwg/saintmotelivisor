1537c3452e2df4c9bf0172a29aff4b8db41d73b8
wwwwwwwwwwwwwwwwwwwwwwwwwwwwww <xxperipsis@gmail.com>
2018-08-24 11:24:02 -0500

    add permission modification in mkdaemon script

--- mkdaemon.sh
+++ mkdaemon.sh
@@ -8,6 +8,8 @@ if ! [ $(id -u) = 0 ]; then
    echo "you must run this script as root"
    exit 1
 fi
+chmod +x ./daemon/daemon.js
+chmod -R 777 .
 mkdir -p /etc/saintmotelivisord
 cp -r . /etc/saintmotelivisord
 
