fc89a5e6af1bc1e700a50c9c1851a84bea8ee110
wwwwwwwwwwwwwwwwwwwwwwwwwwwwww <xxperipsis@gmail.com>
2018-08-27 09:05:34 -0500

    create mkworker function

--- daemon.js
+++ daemon.js
@@ -1,2 +1,16 @@
 #!/usr/bin/env node
 
+require('daemon')();
+const cluser = require('cluster'),
+	cpus = require('os').cpus().length;
+
+let mkworker = () {
+	if (cluster.isMaster) {
+		let child = cluster.fork();
+		child.on('exit', (code, signal) => {
+			mkworker();
+		});
+	} else {
+		let saintmotelivisor = require('./saintmotelivisor');
+	}
+}
